2ab616c97542496dff3deda93dc71cd8bd4f58a1
Zastinian <zastinian@gmail.com>
2023-03-12 19:05:40 -0600

    feat: Util doc

--- util/Util.js
+++ util/Util.js
@@ -1,6 +1,12 @@
-const {Colors} = require("./Constants");
-/* It resolves a color to a hexadecimal value */
+import {Colors} from "./Constants";
+
+/** Utility functions for common tasks */
 class Util {
+  /**
+   * Resolves a color to a hexadecimal value
+   * @param {string|number} color - The color to resolve
+   * @returns {number} The hexadecimal value of the color
+   */
   static resolveColor(color) {
     if (typeof color === "string") {
       color = Colors[color] ?? color.replace("#", "");
@@ -9,6 +15,11 @@ class Util {
     return color;
   }
 
+  /**
+   * Generates an ISO string from a Date object or timestamp
+   * @param {Date|number|null} [date=Date.now()] - The Date object or timestamp to convert to an ISO string
+   * @returns {string|null} The ISO string or null if date is null
+   */
   static generateISOString(date = Date.now()) {
     if (date === null) return null;
     if (date instanceof Date) return date.toISOString();
